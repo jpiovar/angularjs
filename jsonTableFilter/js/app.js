a1988ed071a822b1a450c1ee067a793081227558
jpiovar <jupi61@gmail.com>
2016-03-07 23:04:42 +0100

    rebuild call of conversion

--- jsonTableFilter/js/app.js
+++ jsonTableFilter/js/app.js
@@ -10,13 +10,17 @@ app.controller('MainCtrl',['$scope','$http', function($scope,$http) {
 	$http.get('data/items.json').success(function(data){
       $scope.items = data;
 	  
+	  $scope.convertSI();
+	  
+    });
+	
+	$scope.convertSI = function(){
 	  /* convert input age from string to integer "30" to 30 */
 	  angular.forEach($scope.items, function (item) {		
 		item.age = parseInt(item.age);
 	  });  
 	  
-	  
-    });
+	}
   
 
   
