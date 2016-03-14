982419bda8e7f9e01a13b7f58f3d78ae024cf0d2
jpiovar <jupi61@gmail.com>
2016-03-14 23:20:27 +0100

    json load version http

--- jsonGetPostData/js/app.js
+++ jsonGetPostData/js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('getpost', []);
 
-app.controller('MainCtrl',['$scope','$http', function($scope,$http) {
+app.controller('MainCtrl',['$scope','$http','$templateCache', function($scope,$http,$templateCache) {
 	
 	$scope.getData = function() {debugger;
 	
@@ -25,24 +25,23 @@ app.controller('MainCtrl',['$scope','$http', function($scope,$http) {
 	
 	$scope.postMethod = function(formData1){debugger;
 		var method = 'POST';
-		var url = 'post.php';
-		$scope.codeStatus = '';
+		$scope.url = 'post.php';
+		$scope.code = null;
+		$scope.response = null;
 		$scope.dataPost = '';
 		
-		/*
-		$http({
-			method: method,
-			url: url,
-			data: formData1,
-			headers: {'Content-Type':'application/x-www-form-urlencoded'}
-		})
-		.success(function (response){debugger;
-			$scope.codeStatus = response.status;
-		})
-		.error(function (response){debugger;
-			$scope.codeStatus = response.status || 'chyba';
+		
+		$http({method: $scope.method, url: $scope.url, cache: $templateCache}).
+        then(function(response) {debugger;
+          $scope.status = response.status;
+          $scope.data = response.data;
+        }, function(response) {debugger;
+          $scope.data = response.data || "Request failed";
+          $scope.status = response.status;
 		});
-		*/
+		
+		
+		/*
 		$http.post("post.php",formData1)
 		.success(function(data, status){debugger;
 			$scope.codeStatus = status;
@@ -51,6 +50,7 @@ app.controller('MainCtrl',['$scope','$http', function($scope,$http) {
 		.error(function (data, status){debugger;
 			$scope.codeStatus = status.status || 'chyba';
 		});
+		*/
 	}
 	
 	
