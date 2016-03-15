30656888ae065111b627021dbb417cbdf93389fd
jpiovar <jupi61@gmail.com>
2016-03-15 20:25:59 +0100

    json load post write to another json file

--- jsonGetPostData/post.php
+++ jsonGetPostData/post.php
@@ -5,13 +5,18 @@
 $data = file_get_contents("php://input");
 $postData = json_decode($data,true);
 
+var_dump($postData);
+/*
 echo($postData['id']);
 echo($postData['name']);
 echo($postData['age']);
 echo($postData['quantity']);
 echo($postData['price']);
+*/
 
-
+$fp = fopen('r.json','w');
+fwrite($fp,json_encode($postData));
+fclose($fp);
 
 
 ?>
