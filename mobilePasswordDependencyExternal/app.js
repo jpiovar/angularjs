88bdcb91d0ae00acc8691143312b7c99ecd8d91b
jpiovar <jpiovar@sk.ibm.com>
2016-11-17 21:46:46 +0100

    show hide password last char show for moment

--- mobilePasswordDependencyExternal/app.js
+++ mobilePasswordDependencyExternal/app.js
@@ -1,6 +1,16 @@
 angular.module('mobilePasswordModule', ['mobilePassword'])
-.controller('mainCtrl', function($scope) {
+.controller('mainCtrl', function($scope,$compile) {
 	debugger;
+	
+	$scope.changePwdInput = function(a,b){ debugger;
+		$scope[a];
+		$scope[a+'P'];
+		
+		$scope.changeModel = a;
+		$scope.changeOperation = b;
+		
+		
+	}
 	
     $scope.showHidePwdInput = function(a,b){ debugger;
 		
@@ -19,26 +29,31 @@ angular.module('mobilePasswordModule', ['mobilePassword'])
 				$scope[a+'Temp'].changed='show';
 				
 				$scope[a+'Temp'].value1 = $scope[a+'P'];
-				$scope[a+'Temp'].value2 = $scope[a];
+				//$scope[a+'Temp'].value2 = $scope[a];
 				
 				$scope[a]=$scope[a+'P'];			
 			}
 			
+			if($scope[a].charCodeAt(0)==8226){
+				debugger;
+				$scope[a]=$scope[a+'P'];
+				$scope[a+'Temp'].value1 = $scope[a+'P'];
+			}
+			
 		}
-		
-		
-		
+				
 		if(b=='hide'){
 			
 			if($scope[a+'Temp'].changed=='' || $scope[a+'Temp'].changed=='show'){ debugger;
 				$scope[a+'Temp'].changed='hide';
 				
-				if($scope[a+'Temp'].value2){
-					$scope[a]=$scope[a+'Temp'].value2;
+				var bullets="";
+				for (var i = 0; i < $scope[a].length; ++i) {
+					bullets += String.fromCharCode(8226);
 				}
-				
-				
-							
+				  				
+				$scope[a]=bullets;
+											
 			}
 			
 		}
--- mobilePasswordDependencyExternal/mobilePassword.js
+++ mobilePasswordDependencyExternal/mobilePassword.js
@@ -1,35 +1,51 @@
 angular.module('mobilePassword', [])
-.directive('password', ['$timeout', function($timeout) {
-  function link(scope, element, attrs) {
+.directive('password', ['$timeout', function($timeout) { debugger;
+  function link(scope, element, attrs) {debugger;
     var lastSize = 0;
     var timer;
     var timeout = 200;
     scope[attrs.password] = '';
+	
+	
+	scope.$watch(attrs.ngModel, function(value) {debugger;
+	
 
-    scope.$watch(attrs.ngModel, function(value) {
-      if (value === undefined) {
-        return;
-      }
-      if (lastSize == value.length) {
-        return;
-      }
-      if (lastSize > value.length) {
-        scope[attrs.password] = scope[attrs.password].substring(0, value.length-1);
-      } else {
-        scope[attrs.password] += value.substring(value.length-1, value.length);
-      }
-      var bullets = "";
-      for (var i = 0; i < value.length; ++i) {
-        bullets += String.fromCharCode(8226);
-      }
-      lastSize = value.length;
-      if (! (timer === undefined )) {
-        $timeout.cancel(timer);
-      }
-      timer = $timeout(function(){
-         scope[attrs.ngModel] = bullets;
-       }, timeout);
-    });
+	
+		  if (value === undefined) {
+			return;
+		  }
+		  if (lastSize == value.length) {
+			return;
+		  }
+		  if (lastSize > value.length) {
+			scope[attrs.password] = scope[attrs.password].substring(0, value.length-1);
+		  } else {
+			scope[attrs.password] += value.substring(value.length-1, value.length);
+		  }
+		  
+		  
+		  //var bullets = value;
+		  
+		  //if( scope[scope.changeModel+"Temp"]==undefined || (scope[scope.changeModel+"Temp"] && scope[scope.changeModel+"Temp"]['changed']!='show') ){
+			  
+			  var bullets="";
+			  
+			  for (var i = 0; i < value.length; ++i) {
+				bullets += String.fromCharCode(8226);
+			  }
+		  //}
+		  
+		  lastSize = value.length;
+		  if (! (timer === undefined )) {
+			$timeout.cancel(timer);
+		  }
+		  timer = $timeout(function(){
+			 scope[attrs.ngModel] = bullets;
+		   }, timeout);
+
+	   
+	});
+	
   }
   return {
     restrict: 'A',
