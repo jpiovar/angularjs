7e0c1e5105a95adfe8612cc94b60b8627e28b962
jpiovar <jpiovar@sk.ibm.com>
2017-01-06 23:12:24 +0100

    directive custom

---
+++ directiveCustomElement/script.js
@@ -0,0 +1,28 @@
+angular.module('docsSimpleDirective', [])
+
+.controller('Controller', ['$scope', function($scope) {
+  $scope.customer = {
+    name: 'Naomi',
+    address: '1600 Amphitheatre'
+  };
+}])
+
+.directive('myCustomer', function() {debugger;
+	
+	
+
+  return {	  
+	  
+	link: function(scope, element, attributes){
+
+      console.log(attributes.myCustomer);
+      console.log(attributes.anotherParam);
+
+    },
+	
+	restrict: 'AEC',
+    template: function(elem, attr){debugger;
+		return 'Name: {{customer.name}} Address: {{customer.address}} and '+attr.myCustomer+' and '+attr.anotherParam;
+	}
+  };
+});
