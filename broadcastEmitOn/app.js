b5072e3c05e5392023e83f3da84fe98fd3fc6348
jpiovar <jpiovar@sk.ibm.com>
2017-01-19 19:56:17 +0100

    broadcast to all levels

--- broadcastEmitOn/app.js
+++ broadcastEmitOn/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('appMain', []);
 
 
-app.controller('topCtrl', ['$scope', function($scope) {
+app.controller('topCtrl', ['$scope','$rootScope', function($scope,$rootScope) {
 	
 	$scope.$on('event1', function(evt, args){
 		console.log(args.message + ' in topCtrl');
@@ -10,7 +10,7 @@ app.controller('topCtrl', ['$scope', function($scope) {
 }]);
 
 
-app.controller('middleCtrl', ['$scope', function($scope) {
+app.controller('middleCtrl', ['$scope','$rootScope', function($scope,$rootScope) {
   
 	$scope.middleBroadcast = function(){
 		$scope.$broadcast('event1',{message:'broadcast event1 from middleCtrl'});
@@ -20,13 +20,26 @@ app.controller('middleCtrl', ['$scope', function($scope) {
 		$scope.$emit('event1',{message:'emit event1 from middleCtrl'});
 	}
 	
+	$scope.siblingBroadcast = function(){
+		$rootScope.$broadcast('event1',{message:'broadcast event1 from middleCtrl'});
+	}
+	
 }]);
 
 
-app.controller('lowerCtrl', ['$scope', function($scope) {
+app.controller('lowerCtrl', ['$scope','$rootScope', function($scope,$rootScope) {
 
 	$scope.$on('event1', function(evt, args){
 		console.log(args.message + ' in lowerCtrl');
+	});
+
+}]);
+
+
+app.controller('siblingCtrl', ['$scope','$rootScope', function($scope,$rootScope) {
+
+	$rootScope.$on('event1', function(evt, args){
+		console.log(args.message + ' in siblingCtrl');
 	});
 
 }]);
