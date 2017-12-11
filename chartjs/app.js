eb443b5131ed2e567402c26cc874e5b001798157
Juraj Piovar <jpiovar@sk.ibm.com>
2017-12-11 16:06:11 +0100

    chart js  set color

--- chartjs/app.js
+++ chartjs/app.js
@@ -5,6 +5,16 @@ app.controller('MainCtrl', function($scope) {
 
     $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
     $scope.series = ['Series A', 'Series B'];
+
+    // $scope.colors = [{
+    //     fillColor: 'rgba(47, 132, 71, 0.8)',
+    //     strokeColor: 'rgba(47, 132, 71, 0.8)',
+    //     highlightFill: 'rgba(47, 132, 71, 0.8)',
+    //     highlightStroke: 'rgba(47, 132, 71, 0.8)'
+    // }];
+
+
+    $scope.colors = ['#72C02C', '#ff0000'];
   
     $scope.data = [
       [65, 59, 80, 81, 56, 55, 40],
