fde7397bfd666196ac25267a4eee598387830a65
Juraj Piovar <jpiovar@sk.ibm.com>
2017-12-11 15:52:19 +0100

    chart js bar angular

---
+++ chartjs/app.js
@@ -0,0 +1,30 @@
+var app = angular.module('plunker', ['chart.js']);
+
+app.controller('MainCtrl', function($scope) {
+
+
+    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    $scope.series = ['Series A', 'Series B'];
+  
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+
+   
+
+    $scope.setValues = function(){
+    
+
+        $scope.data = [
+            [70, 10, 90, 81, 56, 55, 40],
+            [3, 2, 4, 19, 86, 27, 90]
+          ];
+        
+
+    }
+
+
+
+
+});
